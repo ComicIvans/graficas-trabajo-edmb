dcc899b074ec6ea4fdce08abb027352f93b4d03c
Iván Salido Cobo <isalidocobo@gmail.com>
2023-10-31 21:36:32 +0100

    Animado

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-from ojos_gato import ojos_gato
+from ojos_gato import ojos_gato, update_ojos_gato_int
 
 if __name__ == "__main__":
-  number = input("Introduce un número: \n0 - Salir \n1 - Ojos de gato \n")
+  number = input("Introduce un número:\n0 - Salir \n1 - Ojos de gato fijo\n2 - Ojos de gato interactivo\n")
 
   if number == "0":
       print("Saliendo...")
       exit()
   elif number == "1":
       ojos_gato()
+  elif number == "2":
+      update_ojos_gato_int()
   else:
       print("¡Número no válido! Saliendo...")
       exit()
--- ojos_gato.py
+++ ojos_gato.py
@@ -1,6 +1,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib as mpl
+import matplotlib.animation as animation
+
+
+# Set the points for the contour plot
+x = np.linspace(-5, 5, 100)
+y = np.linspace(-5, 5, 100)
+X, Y = np.meshgrid(x, y)
 
 def ojos_gato():
   # Ask for c valued
@@ -17,13 +24,21 @@ def ojos_gato():
   if c < 1:
     print('¡Número inferior a 1! Saliendo...')
     return
-
-  x = np.linspace(-5, 5, 100)
-  y = np.linspace(-5, 5, 100)
-  X, Y = np.meshgrid(x, y)
   Z = -np.log(c * np.cosh(Y) + np.sqrt(c**2 - 1) * np.cos(X))
 
   plt.figure(1)
   plt.contour(X, Y, Z)
   plt.gca().set_title(f'Ojos de gato para c = {c}')
+  plt.show()
+
+def update_ojos_gato(i):
+  plt.clf()
+  c = np.linspace(1, 2, 100)[i]
+  Z = -np.log(c * np.cosh(Y) + np.sqrt(c**2 - 1) * np.cos(X))
+  plt.contour(X, Y, Z)
+  plt.gca().set_title(f'Ojos de gato para c = {c}')
+
+def update_ojos_gato_int():
+  fig = plt.figure(1)
+  ani = animation.FuncAnimation(fig, update_ojos_gato, frames=50)
   plt.show()
